a7123e5926d7c0228c2b6033a3d8667dcc6c1340
André <andre.martins@unigran.br>
2024-09-30 15:00:45 -0400

    Exemplo Estatico

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,14 @@
 import 'package:flutter/material.dart';
+
+void main(List<String> args) {
+  runApp(App());
+}
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Center(child: Text("Olá")),
+    );
+  }
+}
