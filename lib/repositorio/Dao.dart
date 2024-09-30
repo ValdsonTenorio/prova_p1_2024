15d58abd533f02823245fbd89530eedceaec0537
André <andre.martins@unigran.br>
2024-09-30 15:58:57 -0400

    criado repositorio

---
+++ lib/repositorio/Dao.dart
@@ -0,0 +1,14 @@
+import 'package:novo_projeto/entidade/pessoa.dart';
+
+class Dao {
+  final List _dados = [];
+  add(Pessoa p) {
+    _dados.add(p);
+  }
+
+  remove(Pessoa p) {
+    _dados.remove(p);
+  }
+
+  get dados => _dados;
+}
