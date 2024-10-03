7d57b11c9661033de7e8cc82770bf15dcdca00b3
André <andre.martins@unigran.br>
2024-10-02 20:16:44 -0400

    Correção rotas

--- lib/listagem.dart
+++ lib/listagem.dart
@@ -21,54 +21,53 @@ class _Listagem extends State<Listagem> {
     return Scaffold(
       body: ListView.builder(
         itemBuilder: (context, indice) {
-          return ListTile(
-            title: Text(widget.pessoaController.info(indice)),
-            trailing: IconButton(
-              icon: const Icon(Icons.delete, color: Colors.red),
-              onPressed: () {
-                // Confirmação antes de excluir
-                showDialog(
-                  context: context,
-                  builder: (context) {
-                    return AlertDialog(
-                      title: const Text('Confirmar Exclusão'),
-                      content: const Text(
-                          'Você tem certeza que deseja excluir este item?'),
-                      actions: [
-                        TextButton(
-                          onPressed: () {
-                            Navigator.of(context).pop(); // Fecha o diálogo
-                          },
-                          child: const Text('Cancelar'),
-                        ),
-                        TextButton(
-                          onPressed: () {
-                            // Remove o item
-                            widget.pessoaController
-                                .remover(widget.pessoaController.get(indice));
-                            setState(() {}); // Atualiza a lista após excluir
-                            Navigator.of(context).pop(); // Fecha o diálogo
-                          },
-                          child: const Text('Excluir'),
-                        ),
-                      ],
+          return GestureDetector(
+              onLongPress: () {},
+              onDoubleTap: () {},
+              child: ListTile(
+                title: Text(widget.pessoaController.info(indice)),
+                trailing: IconButton(
+                  icon: const Icon(Icons.delete, color: Colors.red),
+                  onPressed: () {
+                    // Confirmação antes de excluir
+                    showDialog(
+                      context: context,
+                      builder: (context) {
+                        return AlertDialog(
+                          title: const Text('Confirmar Exclusão'),
+                          content: const Text(
+                              'Você tem certeza que deseja excluir este item?'),
+                          actions: [
+                            TextButton(
+                              onPressed: () {
+                                Navigator.of(context).pop(); // Fecha o diálogo
+                              },
+                              child: const Text('Cancelar'),
+                            ),
+                            TextButton(
+                              onPressed: () {
+                                // Remove o item
+                                widget.pessoaController.remover(
+                                    widget.pessoaController.get(indice));
+                                setState(
+                                    () {}); // Atualiza a lista após excluir
+                                Navigator.of(context).pop(); // Fecha o diálogo
+                              },
+                              child: const Text('Excluir'),
+                            ),
+                          ],
+                        );
+                      },
                     );
                   },
-                );
-              },
-            ),
-          );
+                ),
+              ));
         },
         itemCount: widget.pessoaController.size(),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          Navigator.push(
-              context,
-              MaterialPageRoute(
-                  builder: (context) => Cadastro(
-                        pessoaController: widget.pessoaController,
-                      ))).then((_) {
+          Navigator.pushNamed(context, '/cadastro').then((_) {
             setState(() {}); // Atualiza a lista após voltar
           });
         },
